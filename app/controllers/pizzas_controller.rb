4320de7c8f7637687f41cc83bf1e18c70d699231
Zuadinho <alancostacs@hotmail.com>
2024-11-06 10:43:40 -0300

    CRUD das pizzas

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -4,23 +4,34 @@ class PizzasController < ApplicationController
   end
 
   def new
-
+    @pizza = Pizza.new
   end
 
   def create
+    @pizza = Pizza.new(pizzas_params)
+    if @pizza.save
+      redirect_to pizzas_path
+    else
+      render :new
+    end
 
   end
 
   def edit
+    @pizza=Pizza.find(params[:id])
 
   end
 
   def destroy
-
+    @pizza=Pizza.find(params[:id])
+    @pizza.destroy
+    redirect_to pizzas_path
   end
 
   def update
-
+    @pizza=Pizza.find(params[:id])
+    @pizza.update(pizzas_params)
+    redirect_to pizzas_path
   end
 
   private
